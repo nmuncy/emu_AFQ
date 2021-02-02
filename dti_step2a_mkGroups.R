acc962bfa61a35c1e8a47d1081d6213e2992b633
Nathan Muncy <nmuncy@fiu.edu>
2021-02-02 13:00:22 -0500

    update naming, R

--- dti_step2a_mkGroups.R
+++ dti_step2a_mkGroups.R
@@ -7,5 +7,5 @@ colnames(df_reduced) <- c("Subj", "Sleep", "Anxious", "Pars6", "Pars7")
 df_reduced[,1:5] <- c(df_raw$src_subject_id, df_raw$pinf_random, df_raw$pinf_group, df_raw$pars_6, df_raw$pars_7)
 
 # write
-out_file <- paste0(data_dir, "/Group_data.csv")
-write.table(df_reduced, out_file, quote = F, sep = ",", row.names = F)
+out_file <- paste0(data_dir, "/Group_data.txt")
+write.table(df_reduced, out_file, quote = F, sep = "\t", row.names = F)
