69912385da9512a0421f68615b5d25f51ce6563f
Nathan Muncy <nmuncy@fiu.edu>
2021-02-10 14:39:37 -0500

    started MRtrix preproc script

---
+++ mrtrix_step1.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+### --- Notes:
+#
+# Dependencies: MRtrix3, FSL, ANTs
+#	FSL requires python 2.7
+
+
+projDir=~/Projects/emu_diff/emu_data
+dsetDir=${projDir}/dset
+derivDir=${projDir}/derivatives/mrtrix
+
+sess=ses-S2
+subj=sub-4000
+
+subjData=${dsetDir}/${subj}/${sess}
+subjOut=${derivDir}/${subj}/${sess}
+
+if [ ! -d $subjOut ]; then
+	mkdir -p $subjOut
+fi
+cd $subjOut
+
+# convert to mrtrix format
+if [ ! -f ${subj}_dwi.mif ]; then
+	mrconvert ${subjData}/dwi/${subj}_${sess}_run-1_dwi.nii.gz \
+		dwi.mif \
+		-fslgrad \
+		${subjData}/dwi/${subj}_${sess}_run-1_dwi.bvec \
+		${subjData}/dwi/${subj}_${sess}_run-1_dwi.bval
+fi
+
+# get bvec/als
+for suff in bv{ec,al}; do
+	if [ ! -f ${subj}_dwi.$suff ]; then
+		cp ${subjData}/dwi/${subj}_${sess}_run-1_dwi.$suff dwi.$suff
+	fi
+done
+
+# check num vols/vecs/vals
+numVol=`mrinfo -size dwi.mif | awk '{print $4}'`
+numVec=`awk '{print NF; exit}' dwi.bvec`
+numVal=`awk '{print NF; exit}' dwi.bval`
+if [ $numVol != $numVec ] || [ $numVol != $numVec ]; then
+	echo "issue"
+	exit 1
+fi
+
+# get fmaps, make average
+for dir in AP PA; do
+	if [ ! -f ${subj}_${dir}_fmap.mif ]; then
+		cp ${subjData}/fmap/${subj}_${sess}_acq-dwi_dir-${dir}_run-?_epi.nii.gz fmap_${dir}.nii.gz
+		mrconvert fmap_${dir}.nii.gz fmap_${dir}.mif
+	fi
+done
+
+if [ ! -f fmap_both.mif ]; then
+	mrcat fmap_AP.mif fmap_PA.mif -axis 3 fmap_both.mif
+fi
+
+# denoise
+if [ ! -f dwi_den.mif ]; then
+	dwidenoise dwi.mif dwi_den.mif -noise tmp_noise.mif
+	mrcalc dwi.mif dwi_den.mif -subtract tmp_residual.mif
+fi
+
+# # unwarp data, remove eddy currents
+# dwifslpreproc dwi_den.mif dwi_preproc.mif \
+# 	-nocleanup \
+# 	-pe_dir AP \
+# 	-rpe_pair -se_epi fmap_both.mif \
+# 	-eddy_options " --slm=linear --data_is_shelled"
+
