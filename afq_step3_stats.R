7886305d6ed824d9a16355e2e9ac6dc9f3a63aea
Nathan Muncy <nmuncy@fiu.edu>
2021-02-22 19:28:41 -0500

    GAM working

--- afq_step3_stats.R
+++ afq_step3_stats.R
@@ -1,64 +1,130 @@
 
+
+library("mgcv")
+library("tidymv")
+library("fitdistrplus")
+library("ggplot2")
+
+
 # Orienting vars
-afqDir <- "/Users/nmuncy/Projects/emu_diff/emu_data/derivatives/afq/"
-subjList <- list.files(path=afqDir, pattern="sub-*")
-sess <- "ses-S2"
+dataDir <- "/Users/nmuncy/Projects/emu_diff/analyses/"
 
 
-### --- Step One
-#
-# Make master data frame in long form
-# 
-# Add group membership
+### --- Make dataset
 
-df_master <- as.data.frame(matrix(NA, ncol=5, nrow=1))
-colnames(df_master) <- c("Subj", "Tract", "Node", "FA", "MD")
-
-for(subj in subjList){
-  # subj <- as.character(subjList[1])
-  
-  # get data
-  subjFile <- paste0(afqDir, subj, "/", sess, "/", subj, "_", sess, "_dwi_profiles.csv")
-  df_subj <- read.csv(subjFile, sep = ",", header=T)
-  
-  # prepare, add to master
-  colnames(df_subj) <- colnames(df_master)
-  df_subj$Subj <- subj
-  df_master <- rbind(df_master, df_subj)
-  
-  # make tract plots
-  tractList <- unique(df_subj$tractID)
-  for(tract in tractList){
-    plot(df_subj[which(df_subj$tractID == tract), ]$dti_fa, type = "l", lwd=2)
-    title(main=tract)
-  }
-}
-
-# clean
-df_master <- df_master[-1,]
-row.names(df_master) <- 1:nrow(df_master)
+# Get group data
+df_all <- read.delim(paste0(dataDir, "tract_profiles.csv"), sep = ",", header = T)
+subjList <- unique(df_all$subjectID)
+tractList <- unique(df_all$tractID)
 
 # add anxiety
-refFile <- read.delim("Group_Data.txt", sep="\t", header=T)
-df_master$Group <- NA
+df_group <- read.delim(paste0(dataDir, "Group_data.txt"), sep = "\t", header=T)
+df_all$Group <- NA
 for(subj in subjList){
-  
+
   # determine subj indices
-  ind_master <- which(df_master$Subj == subj)
-  ind_anx <- which(refFile$Subj == gsub(".*-", "", subj))
-  
+  ind_master <- which(df_all$subjectID == subj)
+  ind_anx <- which(df_group$Subj == subj)
+
   # determine group
-  if(refFile[ind_anx,]$Pars7 > 3){
-    h_group <- "Anxious"
-  }else if(refFile[ind_anx,]$Pars7 <= 3){
-    h_group <- "Control"
+  if(df_group[ind_anx,]$Pars7 > 13){
+    h_group <- 1
+  }else if(df_group[ind_anx,]$Pars7 <= 13){
+    h_group <- 0
   }
-  
+
   # fill
-  df_master[ind_master,]$Group <- h_group
+  df_all[ind_master,]$Group <- h_group
 }
+df_all$Group <- factor(df_all$Group)
 
 
+### --- Run model for each tract
+
+# subset
+# for(tract in tractList){
+#   
+# }
+tract <- "UNC_L"
+df_tract <- df_all[which(df_all$tractID == tract), ]
+df_tract$dti_fa <- round(df_tract$dti_fa, 3)
+
+# # plot mean data
+ggplot(data = df_tract) +
+  geom_smooth(mapping = aes(x=nodeID, y=dti_fa, color=Group))
+
+ggplot(data = df_tract) +
+  geom_point(mapping = aes(x=nodeID, y=dti_fa, color=Group),size=0.3) +
+  geom_smooth(mapping = aes(x=nodeID, y=dti_fa, color=Group))
+
+# nodeList <- unique(df_tract$nodeID)
+# df_plot <- as.data.frame(matrix(NA, nrow=2*length(nodeList), ncol=3))
+# colnames(df_plot) <- c("Node", "Group", "FA")
+# df_plot$Node <- rep(nodeList,2)
+# df_plot$Group <- c(rep(0,length(nodeList)), rep(1,length(nodeList)))
+# 
+# for(i in 1:length(nodeList)){
+#   node <- i-1
+#   ind_con <- which(df_plot$Node == node & df_plot$Group == 0)
+#   ind_anx <- which(df_plot$Node == node & df_plot$Group == 1)
+#   df_plot[ind_con, ]$FA <- round(mean(df_tract[which(df_tract$nodeID == node & df_tract$Group == 0), ]$dti_fa), 3)
+#   df_plot[ind_anx, ]$FA  <- round(mean(df_tract[which(df_tract$nodeID == node & df_tract$Group == 1), ]$dti_fa), 3)
+# }
+# 
+# data_con <- df_plot[which(df_plot$Group == 0),]$FA
+# data_anx <- df_plot[which(df_plot$Group == 1),]$FA
+# 
+# plot(data_con, type="l", col="blue")
+# lines(data_anx, type="l", col="red")
+
+# check shape
+descdist(df_tract$dti_fa, discrete=F)
+
+fit.beta <- fitdist(df_tract$dti_fa, "beta")
+plot(fit.beta)
+fit.beta$aic
+
+fit.gamma <- fitdist(df_tract$dti_fa, "gamma")
+plot(fit.gamma)
+fit.gamma$aic
+
+
+# separate model for each group, include diff intercepts
+#   each group gets own smoothness & wiggly parameters
+g <- gam(dti_fa ~ Group + s(nodeID, by = Group), data = df_tract, family = betar(link = "logit"), method = "REML")
+plot(g,pages=1,scheme=1,unconditional=TRUE)
+summary(g)
+gam.check(g, rep = 500)
+
+# # use single basis (fs) - likely the better model
+# #   assumes the groups will have a similar wiggliness,
+# #   closer to group as random effect
+# g <- gam(dti_fa ~ s(nodeID, Group, bs = 'fs'), data = df_tract, method = "REML")
+# plot(g,pages=1,scheme=1,unconditional=TRUE) 
+# summary(g)
+# gam.check(g, rep = 1000)
+
+
+
+### --- Diagnose
+
+# check K (k-index should be ~1)
+g_k <- gam(dti_fa ~ Group + s(nodeID, by = Group, k=30), data = df_tract, family = betar(link = "logit"), method = "REML")
+gam.check(g_k, rep = 1000)
+# plot(g_k,pages=1,scheme=1,unconditional=TRUE)
+plot_smooths(model=g_k, series=nodeID, comparison=Group) + theme(legend.position = "top")
+summary(g_k)
+
+# check fam
+g_f <- gam(dti_fa ~ Group + s(nodeID, by = Group, k=35), data = df_tract, family = Gamma, method = "REML")
+gam.check(g_f, rep = 1000)
+plot_smooths(model=g_f, series=nodeID, comparison=Group) + theme(legend.position = "top")
+summary(g_f)
+
+AIC(g_k, g_f)
+
+# differences
+plot_difference(g_k, series=nodeID, difference = list(Group = c(0, 1)))
 
 
 
