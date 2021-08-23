a6abd9c34f35ac031f43c77a1418ed889c2c13d9
Nathan Muncy <nmuncy@fiu.edu>
2021-08-23 14:14:36 -0400

    updated step4 notes

--- afq_step4_manuscript.R
+++ afq_step4_manuscript.R
@@ -1,25 +1,29 @@
-
-
 library("lsr")
 
-### --- Notes:
+
+# Notes:
 #
 # Quick stats for the manuscript
 
-dataDir <- "/Users/nmuncy/Projects/emu_AFQ/analyses/"
 
-# For group 3 (L v M v H)
-gType <- 3
+# Set Up -------
+data_dir <- "/Users/nmuncy/Projects/emu_AFQ/analyses/"
 tract <- "UNC_L"
 
-dataFile <- paste0(dataDir, "Master_dataframe_G", gType,".csv")
-df_afq <- read.csv(dataFile)
-df_subset <- df_afq[which(df_afq$nodeID == 0 & df_afq$tractID == tract),]
+# Use group 3 (L v M v H) data
+g_type <- 3
 
+# read in data, subset
+data_file <- paste0(data_dir, "Master_dataframe_G", g_type, ".csv")
+df_afq <- read.csv(data_file)
+df_subset <- df_afq[which(df_afq$nodeID == 0 & df_afq$tractID == tract), ]
+
+
+# Get Demographics ------
 num_subj <- dim(df_subset)[1]
 num_female <- length(which(df_subset$Sex == 0))
-age_avg <- round(mean(df_subset$Age),2)
-age_sd <- round(sd(df_subset$Age),2)
+age_avg <- round(mean(df_subset$Age), 2)
+age_sd <- round(sd(df_subset$Age), 2)
 
 stat_age <- aov(Age ~ as.factor(Group), data = df_subset)
 summary(stat_age)
