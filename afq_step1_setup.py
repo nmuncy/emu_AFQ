2070c5cfef3d2fe925b62e3ea03f418357aea54c
Nathan Muncy <nmuncy@fiu.edu>
2021-10-19 12:19:08 -0400

    added documentation, some refactoring

--- afq_step1_setup.py
+++ afq_step1_setup.py
@@ -1,46 +1,113 @@
+"""Setup config.toml file for AFQ
+
+This script will edit the BIDs file dataset_description.json
+with values needed by AFQ. It will then edit the AFQ
+configuration file config.toml with project-specific values.
+
+It is wrapped by afq_step1_submit.sh.
+"""
+
 import os
 import toml
 import json
-import sys
+from argparse import ArgumentParser
 
 
-"""
-Notes:
+def get_args():
+    parser = ArgumentParser("Receive bash args from afq_step1_submit.sh wrapper")
+    parser.add_argument(
+        "-c", "--config-file", help="/path/to/afq/config.toml", type=str, required=True,
+    )
+    parser.add_argument(
+        "-b",
+        "--bids-directory",
+        help="/path/to/bids/project_directory",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "-d",
+        "--deriv-directory",
+        help="/path/to/bids/project_directory/derivatives/pre-processed_dwi",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "-j",
+        "--json-file",
+        help="/path/to/bids/project_directory/dataset_description.json",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "-p",
+        "--preproc-dwi",
+        help="Description str for pre-processed DWI",
+        type=str,
+        default="dwi_preproc",
+    )
+    return parser
 
-This script will edit the BIDs json file dataset_description
-    with values needed by AFQ. It will then edit the toml
-    file config with project-specific values.
-"""
 
+def write_jsons(json_file, dset_json, deriv_json, dwi_str):
+    """Update and write a number of json files.
 
-def main():
+    Three dataset_description.json files will be written/updated: the
+    input json_file, a copy of json_file written to dset_json, and
+    another copy written to deriv_json. These json files will have the
+    "PipelineDescription" field updated/appended for AFQ needs.
 
-    # receive arguments
-    code_dir = str(sys.argv[1])
-    bids_dir = str(sys.argv[2])
-    deriv_dir = str(sys.argv[3])
-
-    # append data_description with new field
-    json_file = os.path.join(deriv_dir, "dataset_description.json")
-    new_field = {"PipelineDescription": {"Name": "dwi_preproc"}}
-
+    Parameters
+    ----------
+    json_file : str
+        The file location of input dataset_description.json
+    dset_json : str
+        The location of dataset_description.json in bids dset, will be
+        created/overwritten with json_file values
+    deriv_json : str
+        The location of dataset_description.json in bids derivatives/dwi,
+        will be created/overwritten with json_file values
+    dwi_str : str
+        String to append as PipelineDescription Name for dwi data
+     """
     with open(json_file) as jf:
         json_content = json.load(jf)
 
-    if "PipelineDescription" not in json_content.keys():
-        json_content.update(new_field)
-
+    json_content.update({"PipelineDescription": {"Name": "General"}})
     with open(json_file, "w") as jf:
         json.dump(json_content, jf)
 
-    # edit config.toml
-    toml_file = os.path.join(code_dir, "config.toml")
+    json_content.update({"PipelineDescription": {"Name": "dcm2niix"}})
+    with open(dset_json, "w") as jf:
+        json.dump(json_content, jf)
 
+    json_content.update({"PipelineDescription": {"Name": f"{dwi_str}"}})
+    with open(deriv_json, "w") as jf:
+        json.dump(json_content, jf)
+
+    jf.close()
+
+
+def main():
+    """Configure json and toml files."""
+    # receive arguments
+    args = get_args().parse_args()
+    toml_file = args.config_file
+    json_file = args.json_file
+    bids_dir = args.bids_directory
+    deriv_dir = args.deriv_directory
+    dwi_str = args.preproc_dwi
+
+    # append and make dataset_description.json files
+    dset_json = os.path.join(bids_dir, "dset", "dataset_description.json")
+    deriv_json = os.path.join(deriv_dir, "dataset_description.json")
+    write_jsons(json_file, dset_json, deriv_json, dwi_str)
+
+    # edit config.toml with desired fields
     toml_dict = toml.load(toml_file)
     toml_dict["TRACTOGRAPHY"]["directions"] = "prob"
     toml_dict["BIDS"]["bids_path"] = bids_dir
     toml_dict["files"]["dmriprep_folder"] = deriv_dir
-
     tf = open(toml_file, "w")
     toml.dump(toml_dict, tf)
     tf.close()
