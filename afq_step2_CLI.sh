a15a619aa3dc7f9c181fdb01a432ecdf5be4868d
Nathan Muncy <nmuncy@fiu.edu>
2021-11-30 14:59:39 -0500

    updated step2 CLI

--- afq_step2_CLI.sh
+++ afq_step2_CLI.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
-#SBATCH --time=40:00:00   # walltime
-#SBATCH --ntasks=9   # number of processor cores (i.e. tasks)
-#SBATCH --nodes=1   # number of nodes
-#SBATCH --mem-per-cpu=24gb   # memory per CPU core
-#SBATCH -J "AFQ"   # job name
-#SBATCH -p IB_44C_512G   # partition name
-#SBATCH --account iacc_madlab  # account
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=24gb
+#SBATCH --job-name=AFQ
+#SBATCH -p IB_44C_512G
+#SBATCH --account iacc_madlab
 #SBATCH --qos pq_madlab
+
+
+function Usage {
+	cat << USAGE
+
+	Run AFQ via pyAFQ CLI on scheduled resources.
+
+	Required Arguments:
+		arg[1] = /path/to/config.toml
+
+	Example Usage:
+		sbatch \\
+            -e err.txt -o out.txt \\
+            afq_step2_CLI.sh /path/to/config.toml
+USAGE
+}
+
+# check usage
+if [[ $# -ne 1 ]]; then
+    Usage
+    exit 1
+fi
 
 config_file=$1
 pyAFQ $config_file --notrack
--- afq_step2_submit.sh
+++ afq_step2_submit.sh
@@ -1,38 +1,33 @@
 #!/bin/bash
 
+
 function Usage {
 	cat << USAGE
 
 	Wraps afq_step2_CLI.sh
 
 	This script will check for files required by AFQ and then
-	sbatch submit afq_step2_CLI.sh. Sbatch stdout/err will be 
+	sbatch submit afq_step2_CLI.sh. Sbatch stdout/err will be
 	captured in a time-stamped directory located at
 	<proc_dir>/derivatives/Slurm_out.
 
-	Usage:
-
-		afq_step2_submit.sh \
-			-c config.toml \
-			-p /path/to/proc_dir
+	Requires:
+		pyAFQ in \$PYTHONPATH
 
 	Required Arguments:
-
-		-c config.toml = AFQ config.toml file 
+		-c config.toml = AFQ config.toml file
 			same file as from afq_step1_setup.py -c <config-file>
 		-p </path/to/proc_dir> = location of BIDS-structured project directory
 			same dir as afq_step1_submit.sh -p <proc_dir>
 
 	Optional Arguments:
-
 		-t </path/to/AFQ_data> = location of AFQ_data template directory
 			useful for HPC environments where AFQ API cannot download needed files
 
 	Example Usage:
-
-		afq_step2_submit.sh \
-			-c ~/compute/emu_AFQ/config.toml \
-			-p /scratch/madlab/emu_AFQ \
+		./afq_step2_submit.sh \\
+			-c ~/compute/emu_AFQ/config.toml \\
+			-p /scratch/madlab/emu_AFQ \\
 			-t /home/data/madlab/atlases/AFQ_data
 USAGE
 }
@@ -41,7 +36,7 @@ USAGE
 unset config_file proj_dir afq_data
 while getopts ":c:p:t:h" OPT
 	do
-	case $OPT in 
+	case $OPT in
 		c) config_file=${OPTARG}
 			;;
 		p) proj_dir=${OPTARG}
@@ -51,6 +46,7 @@ while getopts ":c:p:t:h" OPT
 		h)
 			Usage
 			exit 0
+			;;
 		\?) echo -e "\n \t ERROR: invalid option." >&2
 			exit 1
 			;;
@@ -73,6 +69,14 @@ if [ ! -d $proj_dir ]; then
 	echo -e "\n \t ERROR: $proj_dir not detected or is not a directory." >&2
 	Usage
 	exit 1
+fi
+
+
+# check for pyAFQ
+which pyAFQ > /dev/null 2>&1
+if [ $? != 0 ]; then
+	echo -e "\n \t ERROR: did not find pyAFQ in \$PYTHONPATH" >&2
+	exit 2
 fi
 
 # work
