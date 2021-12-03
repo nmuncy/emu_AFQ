17abce680554223213b3356c021e2ef8b673dedb
Nathan Muncy <nmuncy@fiu.edu>
2021-12-03 12:31:29 -0500

    added plotting

--- afq_step5_traditional.R
+++ afq_step5_traditional.R
@@ -1,4 +1,4 @@
-
+library("ggplot2")
 
 # Notes ----
 #
@@ -33,6 +33,24 @@ stat_switch <- function(df_node, tract){
   return(h_stats)
 }
 
+switch_tract_name <- function(tract) {
+  # Switch for decoding AFQ tract names
+  #
+  # Arguments:
+  #   tract = AFQ tract string
+  #
+  # Returns:
+  #   x_tract = str, reformatted tract name
+  
+  x_tract <- switch(
+    tract,
+    "UNC_L" = "L. Uncinate",
+    "UNC_R" = "R. Uncinate",
+    "FA" = "A. Forceps",
+  )
+  return(x_tract)
+}
+
 
 for (tract in tract_list) {
   
@@ -41,14 +59,30 @@ for (tract in tract_list) {
   df_tract$sex <- factor(df_tract$sex)
   df_tract$group <- factor(df_tract$group)
   
-  # start empty vector, get list of nodes
+  # get list of nodes, start empty stat, plot tables
   node_list <- unique(df_tract$nodeID)
+  
+  # start empty stat table
   col_names <- c("Node", "Est.", "SE", "t.stat", "p.value", "LB", "UB")
   stat_table <- as.data.frame(
     matrix(NA, ncol=length(col_names), nrow=length(node_list))
   )
   colnames(stat_table) <- col_names
   stat_table$Node <- node_list
+  
+  # start empty plot df
+  plot_names <- c("Node", "Group", "Avg", "SE")
+  group_list <- unique(df_tract$group)
+  df_plot <- as.data.frame(
+    matrix(
+      NA, 
+      nrow = length(group_list) * length(node_list), 
+      ncol = length(plot_names)
+    )
+  )
+  colnames(df_plot) <- plot_names
+  df_plot$Node <- rep(node_list, 3)
+  df_plot$Group <- rep(group_list, each = length(node_list))
   
   # Do "point-wise" GLMs
   for (node in node_list) {
@@ -58,7 +92,7 @@ for (tract in tract_list) {
       df_tract$nodeID == node
     ), ]
     
-    # model with GLM, control for sex, sex by PDS interaction
+    # model with GLM, use correct family for each tract
     fit_glm <- stat_switch(df_node, tract)
     
     # get H vs L coefs, conf intervals, fill table
@@ -66,11 +100,75 @@ for (tract in tract_list) {
     h_conf <- suppressMessages(round(confint(fit_glm)[3,], 2))
     ind_node <- which(stat_table$Node == node)
     stat_table[ind_node, 2:7] <- c(h_coef, h_conf)
+    
+    # get avg, se for plotting
+    for (group in group_list) {
+      num_group <- length(which(df_node$group == group))
+      ind_plot <- which(df_plot$Node == node & df_plot$Group == group)
+      df_plot[ind_plot, ]$Avg <- round(
+        mean(df_node[which(df_node$group == group), ]$dti_fa), 4
+      )
+      h_sd <- sd(df_node[which(df_node$group == group), ]$dti_fa)
+      df_plot[ind_plot, ]$SE <- round(h_sd/(sqrt(num_group)), 4)
+    }
+    
   }
   
   # FDR adjust
   stat_table$FDR <- round(p.adjust(stat_table$p.value, method = "fdr"), 4)
   out_file <- paste0(out_dir, "stats_", tract, ".csv")
-  write.table(stat_table, file = out_file, col.names = T, row.names = F, sep = ",")
+  write.table(
+    stat_table, file = out_file, col.names = T, row.names = F, sep = ","
+  )
+  
+  # set plot standard colors
+  h_cols <- c("blue", "darkred", "black")
+  names(h_cols) <- c("0", "1", "2")
+  
+  # plot lines, confidence
+  h_title <- paste(switch_tract_name(tract), "Tract FA Values")
+  p <- ggplot(data = df_plot, aes(x = Node, y = Avg, group = Group)) +
+    geom_line(aes(color = Group)) +
+    geom_ribbon(aes(ymin = Avg - SE, ymax = Avg + SE), alpha = 0.2) +
+    ggtitle(h_title) +
+    ylab("Avg FA") +
+    xlab("Tract Node") +
+    theme(text = element_text(
+      family = "Times New Roman", face = "bold", size = 14
+    ))
+  
+  # draw sig box, apply colors & labels
+  sig_nodes <- which(stat_table$FDR < 0.05)
+  if (length(sig_nodes) > 0) {
+    p + annotate(
+      "rect", 
+      xmin = sig_nodes[1], 
+      xmax = sig_nodes[length(sig_nodes)],
+      ymin = min(df_plot$Avg),
+      ymax = max(df_plot$Avg) + 0.025,
+      alpha = 0.1,
+      fill = "red"
+    ) + scale_color_manual(
+      values = h_cols,
+      breaks = c("0", "1", "2"),
+      labels = c("Low", "Med", "High")
+    )
+  } else {
+    p + scale_color_manual(
+      values = h_cols,
+      breaks = c("0", "1", "2"),
+      labels = c("Low", "Med", "High")
+    )
+  }
+  
+  
+  
+  
+  
+
 }
+
+
+
+
 
