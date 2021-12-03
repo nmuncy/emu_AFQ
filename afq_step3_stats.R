86db943fb73a5d561ab9b963dc674d45696f915c
Nathan Muncy <nmuncy@fiu.edu>
2021-12-03 16:33:39 -0500

    added shading to plot_diff

--- afq_step3_stats.R
+++ afq_step3_stats.R
@@ -783,15 +783,28 @@ plot_spline_diff_pair <- function(gam_model,
   group_a <- switch_plot_values(factor_a, g_type)[[2]][1]
   group_b <- switch_plot_values(factor_b, g_type)[[2]][1]
   
-  # determine sig nodes
+  # determine sig nodes, make list of start and end nodes
   p_summary <- capture.output(plot_diff(gam_model,
             view = "nodeID",
             comp = list(group = c(factor_a, factor_b)),
-            rm.ranef = T,
+            rm.ranef = T
   ))
   sig_regions <- p_summary[10:length(p_summary)]
   sig_regions <- gsub("\\t", "", sig_regions)
   sig_list <- as.list(strsplit(sig_regions, " - "))
+  start_list <- as.numeric(sapply(sig_list, "[[", 1))
+  end_list <- as.numeric(sapply(sig_list, "[[", 2))
+  
+  # determine bottom of plot
+  p_est <- plot_diff(gam_model,
+                     view = "nodeID",
+                     comp = list(group = c(factor_a, factor_b)),
+                     rm.ranef = T,
+                     plot = F
+  )
+  h_min <- min(p_est$est)
+  h_ci <- p_est[which(p_est$est == h_min), ]$CI
+  min_val <- h_min - h_ci
   
   # set output
   png(
@@ -801,29 +814,65 @@ plot_spline_diff_pair <- function(gam_model,
     width = 600, height = 600
   )
   
+  # draw plot, shade sig section
   par(mar = c(5, 5, 4, 2), family = "Times New Roman")
-  capture.output(plot_diff(gam_model,
-    view = "nodeID",
-    comp = list(group = c(factor_a, factor_b)),
-    rm.ranef = T,
-    main = paste0(
-      "Difference Scores, ", group_a, "-", group_b
-    ),
-    ylab = "Est. FA difference",
-    xlab = "Tract Node",
-    cex.lab = 2,
-    cex.axis = 2,
-    cex.main = 2,
-    cex.sub = 1.5
-  ),
-  file = paste0(
-    table_dir,
-    "Table_Diff_",
-    tract, "_", "G",
-    g_type, "_",
-    factor_a, factor_b,
-    ".txt"
-  ))
+  plot_diff(gam_model,
+            view = "nodeID",
+            comp = list(group = c(factor_a, factor_b)),
+            rm.ranef = T,
+            main = paste0(
+              "Difference Scores, ", group_a, "-", group_b
+            ),
+            ylab = "Est. FA difference",
+            xlab = "Tract Node",
+            cex.lab = 2,
+            cex.axis = 2,
+            cex.main = 2,
+            cex.sub = 1.5,
+            col.diff = "red"
+  )
+  for (h_ind in 1:length(start_list)) {
+    polygon(
+      x = c(rep(start_list[h_ind],2), rep(end_list[h_ind], 2)), 
+      y = c(0, min_val, min_val, 0), 
+      col = rgb(1, 0, 0, 0.2), 
+      border = NA
+      )
+  }
+  
+  
+  # # set output
+  # png(
+  #   filename = paste0(
+  #     gam_plot_dir, "Plot_Diff_", tract, "_", "G", g_type, "_pair.png"
+  #   ),
+  #   width = 600, height = 600
+  # )
+  # 
+  # par(mar = c(5, 5, 4, 2), family = "Times New Roman")
+  # capture.output(plot_diff(gam_model,
+  #   view = "nodeID",
+  #   comp = list(group = c(factor_a, factor_b)),
+  #   rm.ranef = T,
+  #   main = paste0(
+  #     "Difference Scores, ", group_a, "-", group_b
+  #   ),
+  #   ylab = "Est. FA difference",
+  #   xlab = "Tract Node",
+  #   cex.lab = 2,
+  #   cex.axis = 2,
+  #   cex.main = 2,
+  #   cex.sub = 1.5,
+  #   col.diff = "red"
+  # ),
+  # file = paste0(
+  #   table_dir,
+  #   "Table_Diff_",
+  #   tract, "_", "G",
+  #   g_type, "_",
+  #   factor_a, factor_b,
+  #   ".txt"
+  # ))
   par(mar = c(5, 4, 4, 2))
   dev.off()
 }
